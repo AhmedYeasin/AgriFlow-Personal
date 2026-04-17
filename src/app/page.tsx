2ca74988c845ac81c0a59833f073c9d84fa87fce
AhmedYeasin <ahmetchatgami@gmail.com>
2026-04-17 12:10:52 +0600

    saving work before pulling from master

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,24 @@ export default function Home() {
       <main className="py-30 px-10">
         <Hero></Hero>
         <State></State>
+=======
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}   
+        <Features></Features>
+        <CTA></CTA>
+=======
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}   
         <Features></Features>
         <CTA></CTA>
 =======
